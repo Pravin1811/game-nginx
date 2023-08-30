ceb9e7a9fc26a0dbf672feae3084382274a1a7fd
Pravin <kandalassan3@gmail.com>
2023-08-30 16:25:38 +0000

    spell correcetion

--- app/login.php
+++ app/login.php
@@ -130,7 +130,7 @@ if(isset($_POST["submit"])){
                 <!-- You can delete the links only if you purchased the pro version. -->
                 <!-- Licensing information: https://bootstrapmade.com/license/ -->
                 <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-                Designed by <a href="login.php">Parvin</a>
+                Designed by <a href="login.php">Pravin</a>
               </div>
 
             </div>
